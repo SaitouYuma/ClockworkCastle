90dab69d0c5fa32bb18d566bef8ecde10a2525bd
shuto20051014 <shuto20051014@gmail.com>
2025-12-02 17:06:40 +0900

    [add]playermoveScriptの作成

---
+++ Assets/Ito/Playermove.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Playermove : MonoBehaviour
+{
+    [SerializeField]float _playerSpeed = 5f;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
