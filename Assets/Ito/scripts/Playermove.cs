5fbae3cb5711e45999a711585e50d79bcdf8a446
shuto20051014 <shuto20051014@gmail.com>
2025-12-09 16:31:04 +0900

    [update]playerのバグ修正

--- Assets/Ito/scripts/Playermove.cs
+++ Assets/Ito/scripts/Playermove.cs
@@ -1,50 +1,51 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Playermove : MonoBehaviour
 {
+    private Vector2 _rayPos;
+    [SerializeField] GameObject _player;
     [SerializeField] float _playerSpeed = 5f;
     [SerializeField] float _playerJump = 10f;
     [SerializeField] int _playerHp = 1;
-    private bool _isGrounded = false;
+    [SerializeField] LayerMask groundLayer;
     Rigidbody2D _rb;
     private float x;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
-
-    void OnCollisionEnter2D(Collision2D col)
+    private bool IsGrounded()
     {
-        if (col.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = true;
-        }
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, groundLayer);
+        return hit.collider != null;
     }
-
-    void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.gameObject.CompareTag("Ground"))
-        {
-            _isGrounded = false;
-        }
-    }
-
     void Update()
     {
         x = Input.GetAxis("Horizontal");
         _rb.linearVelocity = new Vector2(x * _playerSpeed, _rb.linearVelocity.y);
-        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded == true)
+        Debug.Log($"{_rb.linearVelocity} {x}");
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
         {
+            
             _rb.linearVelocity = new Vector2(x * _playerSpeed, _playerJump);
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ItemPickup();
         }
     }
     public void TakeDamage(int damage)
     {
         _playerHp -= damage;
-        if (_playerHp == 0)
+        if (_playerHp <= 0)
         {
             Dead();
         }
+    }
+    void ItemPickup()
+    {
+        //ƒAƒCƒeƒ€ˆ—
     }
     void Dead()
     {
