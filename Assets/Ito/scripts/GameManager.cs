21753d3262d28aba5bdbd0572acd77619d6aa5b2
shuto20051014 <shuto20051014@gmail.com>
2025-12-22 19:44:19 +0900

    [update]gamemanagerの途中経過

--- Assets/Ito/scripts/GameManager.cs
+++ Assets/Ito/scripts/GameManager.cs
@@ -3,8 +3,44 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    [SerializeField] int _playerlife = 3;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    public enum GameState
+    {
+        Title,
+        Playing,
+        Paused,
+        Result,
+        GameOver
+    }
+   
     public void SceneChanger(int number)
     {
         SceneManager.LoadScene(number);
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif  
+    }
+    public void Gameover()
+    {
+        SceneChanger(0);//ゲームオーバーシーンへ
     }
 }
