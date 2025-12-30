7fd6e51fe8ebc6df96eab00841e87ad024446d9d
shuto20051014 <shuto20051014@gmail.com>
2025-12-30 21:37:42 +0900

    [add]playerのanimationの追加

--- Assets/Ito/scripts/Playermove.cs
+++ Assets/Ito/scripts/Playermove.cs
@@ -11,12 +11,16 @@ public class Playermove : MonoBehaviour
     [SerializeField] float _playerJump = 10f;
     [SerializeField] int _playerHp = 1;
     [SerializeField] LayerMask groundLayer;
+    Animator _anim;
     Rigidbody2D _rb;
     private float x;
+    float _yvelo;
     DeviceController device;
+    bool _grounded;
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponentInChildren<Animator>();
     }
    
     bool IsGrounded()
@@ -26,13 +30,31 @@ public class Playermove : MonoBehaviour
     }
     void Update()
     {
+        _yvelo = _rb.linearVelocity.y;
         CheckGround();
         x = Input.GetAxis("Horizontal");
-        _rb.linearVelocity = new Vector2(x * _playerSpeed, _rb.linearVelocity.y);
+        _rb.linearVelocity = new Vector2(x * _playerSpeed, _yvelo);
+        _anim.SetFloat("Speed", Mathf.Abs(x));//走るアニメーションの処理
         if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() == true)
         {
             _rb.linearVelocity = new Vector2(x * _playerSpeed, _playerJump);
+            _anim.SetTrigger("Jump");
         }
+
+        _anim.SetFloat("Yvelocity", _yvelo);//落ちるアニメーションの処理
+
+        _grounded = IsGrounded();
+        _anim.SetBool("IsGrounded", _grounded);//アニメーションに使う接地判定のbool
+
+        if(x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if(x > 0)
+        {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             ItemPickup();
