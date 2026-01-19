6e0e6997d8963456b758d53d34da2a97ecebeacd
yuma12sa <yuma12sa@gmail.com>
2026-01-19 19:02:02 +0900

    [update]バグ修正

--- Assets/Saito/scripts/GravitySwitch.cs
+++ Assets/Saito/scripts/GravitySwitch.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class GravitySwitch : MonoBehaviour
 {
+    // シングルトン
+    public static GravitySwitch Instance { get; private set; }
+
     [SerializeField] private bool isOn;
 
     // 外部から読み取り専用で状態を確認できる
@@ -9,6 +12,19 @@ public class GravitySwitch : MonoBehaviour
 
     // 外部から状態を確認できる（従来の方法）
     public bool IsOn => isOn;
+
+    private void Awake()
+    {
+        // シングルトンの設定
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void Toggle()
     {
