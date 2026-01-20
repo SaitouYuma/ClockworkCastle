669ce4ba0da9e662011570e4c5bf236f1a8eee2c
shuto20051014 <shuto20051014@gmail.com>
2026-01-20 15:08:23 +0900

    [update]playerの重力反転中のバグ修正

--- Assets/Ito/scripts/GameManager.cs
+++ Assets/Ito/scripts/GameManager.cs
@@ -48,10 +48,14 @@ public class GameManager : MonoBehaviour
     {
         _playerstock--;
         Debug.Log("残りストックは" + _playerstock);
-       // Hpupdate();
+        Hpupdate();
         if (_playerstock > 0)
         {
             Respawn();
+            if (GravitySwitch.Instance != null && GravitySwitch.Instance.IsGravityReversed)
+            {
+                GravitySwitch.Instance.SetOn(false);
+            }
         }
         else
         {
