d60e2cfd2824a304d067df0809d10f14cc432c27
shuto20051014 <shuto20051014@gmail.com>
2026-01-26 06:17:20 +0900

    [add]Sedataの作成

---
+++ Assets/Ito/scripts/SEdata.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SEData
+{
+    public string key;       // ©•ª‚ÅŒˆ‚ß‚é–¼‘O
+    public AudioClip clip;   // ÀÛ‚Ì‰¹º
+    public float volume = 1f;
+}
