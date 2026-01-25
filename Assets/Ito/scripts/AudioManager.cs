4a2e64b6f1b4c2a39e9c56362e398c2a5185e488
shuto20051014 <shuto20051014@gmail.com>
2026-01-26 06:18:05 +0900

    [add]サウンドマネージャーの作成

--- Assets/Ito/scripts/AudioManager.cs
+++ Assets/Ito/scripts/AudioManager.cs
@@ -1,23 +1,87 @@
-using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance { get; set; }
-    [SerializeField] AudioSource BGM;
-    [SerializeField] AudioSource SE;
+    public static AudioManager instance;
 
+    [Header("AudioSource")]
+    public AudioSource _bgmSource;
+    public AudioSource _seSource;
 
-    private void Awake()
+    [Header("BGM List")]
+    public List<BGMData> _bgmList;
+
+    [Header("SE List")]
+    public List<SEData> _seList;
+
+    Dictionary<string, BGMData> _bgmDict = new Dictionary<string, BGMData>();
+    Dictionary<string, SEData> _seDict = new Dictionary<string, SEData>();
+
+    void Awake()
     {
-        // ÉVÉìÉOÉãÉgÉìÇÃê›íË
-        if (Instance == null)
+        if (instance == null)
         {
-            Instance = this;
+            instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
             Destroy(gameObject);
         }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (var data in _bgmList)//BGMÇìoò^
+        {
+            if (data.clip != null)
+                _bgmDict[data.key] = data;
+        }
+
+        foreach (var data in _seList)//SEÇìoò^
+        {
+            if (data.clip != null)
+                _seDict[data.key] = data;
+        }
+    }
+
+    public void PlayBGM(string name)
+    {
+        if (!_bgmDict.ContainsKey(name))
+            return;
+
+        var data = _bgmDict[name];
+
+        if (_bgmSource.clip == data.clip && _bgmSource.isPlaying)
+            return;
+
+        _bgmSource.clip = data.clip;
+        _bgmSource.loop = data.loop;
+        _bgmSource.volume = data.volume;
+        _bgmSource.Play();
+    }
+
+    public void StopBGM()
+    {
+        _bgmSource.Stop();
+    }
+
+    public void PlaySE(string name)
+    {
+        if (!_seDict.ContainsKey(name))
+            return;
+
+        var data = _seDict[name];
+        _seSource.PlayOneShot(data.clip,data.volume);
+    }
+
+    public void SetBGMVolume(float volume)
+    {
+        _bgmSource.volume = volume;
+    }
+
+    public void SetSEVolume(float volume)
+    {
+        _seSource.volume = volume;
     }
 }
