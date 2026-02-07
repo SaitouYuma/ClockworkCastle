b37c38d65b99de434df6c7e27b36d1ec5552d498
shuto20051014 <shuto20051014@gmail.com>
2026-02-07 14:51:47 +0900

    [fix]バグ修正

--- Assets/Ito/scripts/BossBGM.cs
+++ Assets/Ito/scripts/BossBGM.cs
@@ -5,22 +5,15 @@ public class BossBGM : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        AudioManager.instance.StopBGM();
-        StartCoroutine(BossArea());
+        if (!collision.CompareTag("Player")) return;
+
+        AudioManager.instance.PlayBGM("Boss");
     }
+
     private void OnTriggerExit2D(Collider2D collision)
     {
-        AudioManager.instance.StopBGM();
-        StartCoroutine(ExitBossArea());
-    }
-    IEnumerator BossArea()
-    {
-        AudioManager.instance.PlayBGM("Boss");
-        yield return new WaitForSeconds(1f);
-    }
-    IEnumerator ExitBossArea()
-    {
+        if (!collision.CompareTag("Player")) return;
+
         AudioManager.instance.PlayBGM("IngameScene");
-        yield return new WaitForSeconds(1f);
     }
 }
